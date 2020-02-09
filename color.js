f300e1dce3a33f43141b914d3c39b75bdb6a2a7d
Festus Ebin <ebinfestus@gmail.com>
2020-02-09 18:36:02 +0100

    first commit on background changer

---
+++ color.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("#bg");
+
+btn.addEventListener("click", function () {
+    let myColor = "rgb(" + random(255) + ", " + random(255) + ", " + random(255) + ")";
+    document.body.style.backgroundColor = myColor;
+})
+
+function random(num) {
+    return Math.floor(Math.random() * (num + 1))
+}
